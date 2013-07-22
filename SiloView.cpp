17e6c77a4e56912f16274f7f74088675517e1652
Tzu-ping Chung <uranusjr@gmail.com>
2013-07-23 04:13:15 +0800

    Use System font in SiloView buttons

--- SiloView.cpp
+++ SiloView.cpp
@@ -42,8 +42,7 @@ SiloView::SiloView(Silo *silo, QWidget *parent) :
     _backgroundHolder->setStyleSheet(styleSheet);
     QString buttonStyle =
             "border: 1px solid black; border-radius: 5px; background: #87b4ff; "
-            "color: black; font-weight: bold; font-size: 11pt; min-width: 4em; "
-            "font-family: \"Times New Roman\";";
+            "color: black; font-weight: bold; font-size: 11pt; min-width: 4em;";
 
     // Setup signal mapper for silo buttons
     QSignalMapper *mapper = new QSignalMapper(this);
